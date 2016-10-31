5bc80081b9c771acaa346223aa300fa558ea00c5
Martin Elias <melias122@gmail.com>
2016-10-31 18:26:45 +0100

    run.sh for local testing with remote db

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export DATABASE_URL=postgres://nckyuoddcrdjhj:nFr33mACxHcF3_c-hNk777zP5u@ec2-174-129-209-53.compute-1.amazonaws.com:5432/de61bf03f8th10
+
+export PORT=8080
+
+go run main.go
