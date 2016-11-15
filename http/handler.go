ce188584b73ae3f4167762843bc1021322f8f5be
Martin Elias <melias122@gmail.com>
2016-11-15 21:08:37 +0100

    add error hadling and implement middlewares

--- http/handler.go
+++ http/handler.go
@@ -3,49 +3,41 @@ package http
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
 
 	"github.com/BestPrice/backend/bp"
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
 )
 
-// type Handler func(rw http.ResponseWriter, req *http.Request) error
+type handlerFunc func(rw http.ResponseWriter, req *http.Request) error
 
-// type ErrorHandler struct {
-// 	H Handler
-// }
-
-// func (h ErrorHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-// 	err := h.H(rw, req)
-// 	switch err.(type) {
-// 	default:
-// 		log.Println(err)
-// 	}
-// }
-
-type BestpriceHandler struct {
-	r *httprouter.Router
-
-	Service bp.Service
+type statusError struct {
+	error
+	status int
 }
 
-func NewHandler(service bp.Service) *BestpriceHandler {
-	h := &BestpriceHandler{
-		r:       httprouter.New(),
-		Service: service,
+type errorHandler handlerFunc
+
+func (h errorHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if err := h(rw, req); err != nil {
+		log.Println(err)
+		switch err.(type) {
+		case statusError:
+		default:
+			code := http.StatusInternalServerError
+			http.Error(rw, http.StatusText(code), code)
+		}
 	}
-	h.r.GET("/categories", h.categories)
-	h.r.GET("/chainstores", h.chainstores)
-	h.r.GET("/products", h.products)
-	h.r.GET("/stores", h.stores)
-	h.r.POST("/shop", h.shop)
-	h.r.GET("/help", h.api)
-	return h
 }
 
-func (h *BestpriceHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+type accessControlHandler struct {
+	http.Handler
+}
+
+func (h accessControlHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	if origin := req.Header.Get("Origin"); origin != "" {
 		rw.Header().Set("Access-Control-Allow-Origin", origin)
 		rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
@@ -56,78 +48,81 @@ func (h *BestpriceHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) 
 	if req.Method == "OPTIONS" {
 		return
 	}
-
-	h.r.ServeHTTP(rw, req)
+	h.Handler.ServeHTTP(rw, req)
 }
 
-func (h *BestpriceHandler) categories(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	v, err := h.Service.Categories()
-	if err != nil {
-		log.Println(err)
-	}
+type Handler struct {
+	*mux.Router
+	Service bp.Service
+}
 
+func NewHandler(service bp.Service) http.Handler {
+	h := &Handler{
+		Router:  mux.NewRouter(),
+		Service: service,
+	}
+	h.Handle("/categories", errorHandler(h.categories)).Methods(http.MethodGet)
+	h.Handle("/chainstores", errorHandler(h.chainstores)).Methods(http.MethodGet)
+	h.Handle("/products", errorHandler(h.products)).Methods(http.MethodGet)
+	h.Handle("/stores", errorHandler(h.stores)).Methods(http.MethodGet)
+	// h.HandleFunc("/shop", h.shop).Methods(http.MethodPost)
+	h.Handle("/help", errorHandler(h.api)).Methods(http.MethodGet)
+
+	return &accessControlHandler{h}
+}
+
+func encodeJSON(w io.Writer, v interface{}) error {
 	e := json.NewEncoder(w)
 	e.SetIndent("", "\t")
-	err = e.Encode(v)
-	if err != nil {
-		log.Println(err)
-	}
+	return e.Encode(v)
 }
 
-func (h *BestpriceHandler) chainstores(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func (h Handler) categories(w http.ResponseWriter, r *http.Request) error {
+	v, err := h.Service.Categories()
+	if err != nil {
+		return err
+	}
+	return encodeJSON(w, v)
+}
+
+func (h Handler) chainstores(w http.ResponseWriter, r *http.Request) error {
 	v, err := h.Service.Chainstores()
 	if err != nil {
 		log.Println(err)
 	}
-
-	e := json.NewEncoder(w)
-	e.SetIndent("", "\t")
-	err = e.Encode(v)
-	if err != nil {
-		log.Println(err)
-	}
+	return encodeJSON(w, v)
 }
 
-func (h *BestpriceHandler) products(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	log.Println(r.URL)
+func (h Handler) products(w http.ResponseWriter, r *http.Request) error {
 
 	phrase, err := url.QueryUnescape(r.URL.Query().Get("search"))
 	if err != nil {
-		log.Println(err)
+		return err
 	}
 
 	category, _ := bp.NewID(r.URL.Query().Get("category"))
 
 	v, err := h.Service.Products(category, phrase)
 	if err != nil {
-		log.Println(err)
+		return err
 	}
 
-	e := json.NewEncoder(w)
-	e.SetIndent("", "\t")
-	err = e.Encode(v)
-	if err != nil {
-		log.Println(err)
-	}
+	return encodeJSON(w, v)
 }
 
-func (h *BestpriceHandler) stores(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func (h Handler) stores(w http.ResponseWriter, r *http.Request) error {
 	v, err := h.Service.Stores("", "", "")
 	if err != nil {
-		log.Println(err)
+		return err
 	}
-
-	err = json.NewEncoder(w).Encode(v)
-	if err != nil {
-		log.Println(err)
-	}
+	return encodeJSON(w, v)
 }
 
-func (h *BestpriceHandler) shop(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
+func (h Handler) shop(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
 
-func (h *BestpriceHandler) api(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func (h Handler) api(w http.ResponseWriter, r *http.Request) error {
 	var (
 		buf bytes.Buffer
 		enc = json.NewEncoder(&buf)
@@ -154,5 +149,6 @@ func (h *BestpriceHandler) api(w http.ResponseWriter, r *http.Request, _ httprou
 	buf.WriteString("\n\nTODO: POST /shop\n")
 	// enc.Encode([]bp.Store{bp.Store{}, bp.Store{}})
 
-	buf.WriteTo(w)
+	_, err := buf.WriteTo(w)
+	return err
 }
